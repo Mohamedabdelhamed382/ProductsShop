44168c41be1ad881ac2c4e23c5476fcf398db0d6
Mohamed Abd-Elhamed Ahmed <73020992+Mohamedabdelhamed382@users.noreply.github.com>
2022-08-03 13:50:40 +0200

    change name of ex view

--- ProductsShop/Core/Extention/TextField + IBInspectable.swift
+++ ProductsShop/Core/Extention/TextField + IBInspectable.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 @IBDesignable
-class  CustomTextField: UITextField {
+class  TextFieldCustomView: UITextField {
     
     @IBInspectable var leftImage: UIImage? {
         didSet {
